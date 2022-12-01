92a1d0b60a78c71597e482ccd0c52c9f0db5ecc9
shotgun007 <113828769+shotgun007@users.noreply.github.com>
2022-12-01 10:59:12 +0100

    Add files via upload

---
+++ 028FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("How high should the program count?");
+int maxNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("The multiples of the number you enter will be substituted with (Fizz)!");
+int fizz = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("The multiples of the number you enter will be substituted with (Buzz)!");
+int buzz = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+for (int i = 1; i <= maxNumber; i++)
+{
+    if (i % fizz == 0 && i % buzz == 0)
+    {
+        Console.Write("FizzBuzz");
+    }
+    else if (i % fizz == 0 && i % buzz != 0)
+    {
+        Console.Write("Fizz");
+    }
+    else if (i % fizz != 0 && i % buzz == 0)
+    {
+        Console.Write("Buzz");
+    }
+    else
+    {
+        Console.Write(i);
+    }
+    Console.Write(" ");
+}
---
+++ 029FindingTheOperator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Which number should be the first operant?");
+double operant1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Which number should be the second operant?");
+double operant2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Which number should be the result?");
+double result = double.Parse(Console.ReadLine()!);
+
+double Exponentiation = Math.Pow(operant1, operant2);
+double Exponentiation1 = Math.Pow(operant2, operant1);
+if (operant1 + operant2 == result)
+{
+    Console.WriteLine("The operator is +!");
+}
+else if (operant1 - operant2 == result || operant2 - operant1 == result)
+{
+    Console.WriteLine("The operator is -!");
+}
+else if (operant1 * operant2 == result)
+{
+    Console.WriteLine("The operator is *!");
+}
+else if (operant1 / operant2 == result || operant2 / operant1 == result)
+{
+    Console.WriteLine("The operator is /!");
+}
+else if (operant1 % operant2 == result || operant2 % operant1 == result)
+{
+    Console.WriteLine("The operator is %!");
+}
+else if (Exponentiation == result || Exponentiation1 == result)
+{
+    Console.WriteLine("The operator is Exponentiation!");
+}
